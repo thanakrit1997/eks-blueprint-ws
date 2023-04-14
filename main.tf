37a475a96378c3c9f42cc3b1129fde3c3b81ed57
Dasapich Thongnopnua <dasapich@amazon.com>
2023-04-14 18:11:40 +0700

    Enable Kubecost add-on

--- main.tf
+++ main.tf
@@ -173,6 +173,7 @@ module "kubernetes_addons" {
   karpenter_node_iam_instance_profile        = module.karpenter.instance_profile_name
   karpenter_enable_spot_termination_handling = true
   #karpenter_sqs_queue_arn                    = module.karpenter.queue_arn  
+  enable_kubecost                            = true
 
 }
 
