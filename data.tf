0a23a49c3eec220bf28d87ea6e05c33ceace1d1a
Dasapich Thongnopnua <dasapich@amazon.com>
2023-04-14 15:06:46 +0700

    Update Terraform files to create EKS cluster

--- data.tf
+++ data.tf
@@ -8,3 +8,16 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks_blueprints.eks_cluster_id
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks_blueprints.eks_cluster_id
+}
+
+# To Authenticate with ECR Public in eu-east-1
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.virginia
+}
