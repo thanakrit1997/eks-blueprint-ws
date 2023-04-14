0f2ce53754848c73ad8f3cf4289a68a21ea5cda5
Dasapich Thongnopnua <dasapich@amazon.com>
2023-04-14 16:12:07 +0700

    Terraform files to bootstrap ArgoCD for GitOps

--- main.tf
+++ main.tf
@@ -131,3 +131,42 @@ module "vpc" {
 
     tags = local.tags
 }
+
+module "kubernetes_addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.27.0/modules/kubernetes-addons"
+
+  eks_cluster_id     = module.eks_blueprints.eks_cluster_id
+
+  #---------------------------------------------------------------
+  # ARGO CD ADD-ON
+  #---------------------------------------------------------------
+
+  enable_argocd         = true
+  argocd_manage_add_ons = true # Indicates that ArgoCD is responsible for managing/deploying Add-ons.
+
+  argocd_applications = {
+    addons    = local.addon_application
+    #workloads = local.workload_application #We comment it for now
+  }
+
+  argocd_helm_config = {
+    set = [
+      {
+        name  = "server.service.type"
+        value = "LoadBalancer"
+      }
+    ]
+  }
+
+  #---------------------------------------------------------------
+  # ADD-ONS - You can add additional addons here
+  # https://aws-ia.github.io/terraform-aws-eks-blueprints/add-ons/
+  #---------------------------------------------------------------
+
+
+  enable_aws_load_balancer_controller  = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
+  enable_aws_for_fluentbit             = true
+  enable_metrics_server                = true
+
+}
